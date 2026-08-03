9a7142c1264cafcf918608c3b437638c558d8b95
Huilerm <guiilopes1147@gmail.com>
2026-08-03 13:30:04 -0300

    Erros de duplicações corrigidas

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,9 @@ import userRouter from "./routes/user.routes.js";
 import adminRouter from "./routes/admin.routes.js";
 import { AppError } from "./errors/AppError.js";
 import inscricaoRouter from "./routes/inscricao.routes.js";
-import { courseRoutes } from "./routes/course.routes.js";
-import instituicaoRoutes from "./routes/instituicao.routes.js";
 import { professorRoutes } from "./routes/professor.routes.js";
 import relatorioRouter from "./routes/relatorio.routes.js";
-import { courseRoutes } from "./routes/course.routes";
-import documentoRouter from "./routes/documento.routes";
+import { courseRoutes } from "./routes/course.routes.js";
 import instituicaoRoutes from "./routes/instituicao.routes.js";
 import documentoRouter from "./routes/documento.routes.js";
 
